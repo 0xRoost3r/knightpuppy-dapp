00c14251c436d81e6d855664a4d290a0860d789e
Vo Thuong Nguyen <9839951+0xRoost3r@users.noreply.github.com>
2024-05-10 22:53:55 +0700

    WIP: Custom animation Sprite

--- components/flappypuppy/Puppy.tsx
+++ components/flappypuppy/Puppy.tsx
@@ -1,13 +1,43 @@
-import React from "react";
-import { AnimatedSprite, Container } from "@pixi/react";
+import React, { useState } from "react";
+import { AnimatedSprite, Container, Sprite, useTick } from "@pixi/react";
 import { useStore } from "~~/services/store/game";
 
 export function Puppy() {
   const {birdSize,  birdR, birdX, birdY } = useStore();
+  const [currentImageIndex, setCurrentImageIndex] = useState(0);
+  const [frame, setFrame] = useState(0);
+  const [imageIndex, setImageIndex] = useState(0);
+
+  // Define your array of images
+  const pipeImages = [
+    "/images/game/flappypuppy/puppy/puppy1.png",
+    "/images/game/flappypuppy/puppy/puppy2.png",
+    "/images/game/flappypuppy/puppy/puppy3.png",
+    "/images/game/flappypuppy/puppy/puppy4.png"
+  ];
+
+  // Use useTick to change the image every 10 milliseconds
+  useTick((deltaTime) => {
+    const newFrame = frame + 1;
+    if (newFrame > 12) {
+      setFrame(0);
+
+      if (imageIndex > 3) {
+        setImageIndex(0);
+      } else {
+        setImageIndex(imageIndex + 1);
+      }
+      
+    } else {
+      setFrame(newFrame)
+    }
+  });
 
   return (
     <Container position={[birdX, birdY]}>
-      <AnimatedSprite
+      <Sprite anchor={0.5} rotation={birdR} image={pipeImages[imageIndex]} width={birdSize.width} height={birdSize.height}></Sprite>
+
+      {/* <AnimatedSprite
         rotation={birdR}
         width={birdSize.width}
         height={birdSize.height}
@@ -16,7 +46,7 @@ export function Puppy() {
         loop={true}
         anchor={0.5}
         images={["/images/game/flappypuppy/puppy/puppy1.png", "/images/game/flappypuppy/puppy/puppy2.png", "/images/game/flappypuppy/puppy/puppy3.png", "/images/game/flappypuppy/puppy/puppy4.png"]}
-        />
+        /> */}
         {/* <AnimatedSprite
         width={birdSize.width}
         height={birdSize.height}
